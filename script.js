5afe521a04171c7bbf02fea313b47561f7bb7cdc
ritesh kumar <166797186+ritesh-kr22@users.noreply.github.com>
2026-04-25 16:42:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function login() {
+    fetch("http://127.0.0.1:5000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("result").innerText = data.message;
+    });
+}
