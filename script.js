9144a98cc1e685da06f82ddf675754544722ae4b
sachinkr841 <krsachin103@gmail.com>
2026-04-25 21:25:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-function login() {
-    fetch("http://127.0.0.1:5000/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: document.getElementById("username").value,
-            password: document.getElementById("password").value
-        })
-    })
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById("result").innerText = data.message;
-    });
+const ctx = document.getElementById('chart');
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
+    datasets: [{
+      label: 'Delhi AQI',
+      data: [300,320,310,330,350,340,320],
+      borderColor: 'purple',
+      fill: false
+    }]
+  }
+});
+
+if (data.message === "Login success") 
+{
+   window.location.href = "'index.html";
 }
+
