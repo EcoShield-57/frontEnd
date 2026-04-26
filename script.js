cc106231cebfac3de96e92615a6ad27146bf7df3
ritesh kumar <166797186+ritesh-kr22@users.noreply.github.com>
2026-04-26 17:39:55 +0530

    small update

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const ctx = document.getElementById('chart');
-
-new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
-    datasets: [{
-      label: 'Delhi AQI',
-      data: [300,320,310,330,350,340,320],
-      borderColor: 'purple',
-      fill: false
-    }]
-  }
-});
-
-if (data.message === "Login success") 
-{
-   window.location.href = "'index.html";
+function login() {
+    fetch("http://127.0.0.1:5000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("result").innerText = data.message;
+    });
 }
 
+
+// hamburger
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
